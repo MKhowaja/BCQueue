b0fef74c383437591143a1854d468a4242f1fe04
Clement Hoang <clement.hoang24@gmail.com>
2013-08-07 02:16:42 -0400

    Main Menu Navigation Working, Button Styles Fixed

--- BCQueue/ViewModels/MainViewModel.cs
+++ BCQueue/ViewModels/MainViewModel.cs
@@ -11,13 +11,20 @@ namespace BCQueue.ViewModels
 
         #region Static Instances of ViewModels
 
-        //Static Instances of CPBaseViewModel
+        //Static Instance of CPBaseViewModel
         public readonly static CreateProfileVM.CPBaseViewModel _cPBaseViewModel = new CreateProfileVM.CPBaseViewModel();
         
-
         //Static Instances in the root ViewModels folder
         public readonly static HomeViewModel _homeViewModel = new HomeViewModel();
         public readonly static StartViewModel _startViewModel = new StartViewModel();
+      
+        //Static Instances in ViewModels/MainMenuVM/
+        public readonly static MainMenuVM.MMAboutVM _mMAboutVM = new MainMenuVM.MMAboutVM();
+        public readonly static MainMenuVM.MMAddToQueueVM _mMAddToQueueVM = new MainMenuVM.MMAddToQueueVM();
+        public readonly static MainMenuVM.MMConfigureClubProfileVM _mMConfigureClubProfileVM = new MainMenuVM.MMConfigureClubProfileVM();
+        public readonly static MainMenuVM.MMPlayerSignInVM _mMPlayerSignInVM = new MainMenuVM.MMPlayerSignInVM();
+        public readonly static MainMenuVM.MMViewActiveGamesVM _mMViewActiveGamesVM = new MainMenuVM.MMViewActiveGamesVM();
+        public readonly static MainMenuVM.MMViewPlayerProfilesVM _mMViewPlayerProfilesVM = new MainMenuVM.MMViewPlayerProfilesVM();
 
         #endregion 
 
@@ -37,17 +44,55 @@ namespace BCQueue.ViewModels
         }
         
         public ICommand HomeViewCommand { get; private set; }
+        public ICommand MMAboutViewCommand { get; private set; }
+        public ICommand MMAddToQueueViewCommand { get; private set; }
+        public ICommand MMConfigureCPViewCommand { get; private set; }
+        public ICommand MMPlayerSignInViewCommand { get; private set; }
+        public ICommand MMViewActiveGamesViewCommand { get; private set; }
+        public ICommand MMViewPPViewCommand { get; private set; }
 
         private void ExecuteHomeViewCommand()
         {
             CurrentViewModel = MainViewModel._homeViewModel;
         }
+        private void ExecuteMMAboutViewCommand()
+        {
+            CurrentViewModel = MainViewModel._mMAboutVM;
+        }
+        private void ExecuteMMAddToQueueViewCommand()
+        {
+            CurrentViewModel = MainViewModel._mMAddToQueueVM;
+        }
+        private void ExecuteMMConfigureCPViewCommand()
+        {
+            CurrentViewModel = MainViewModel._mMConfigureClubProfileVM;
+        }
+        private void ExecuteMMPlayerSignInViewCommand()
+        {
+            CurrentViewModel = MainViewModel._mMPlayerSignInVM;
+        }
+        private void ExecuteMMViewActiveGamesViewCommand()
+        {
+            CurrentViewModel = MainViewModel._mMViewActiveGamesVM;
+        }
+        private void ExecuteMMViewPPViewCommand()
+        {
+            CurrentViewModel = MainViewModel._mMViewPlayerProfilesVM;
+        }
+
      
 
         public MainViewModel()
         {
             CurrentViewModel = MainViewModel._startViewModel;
+
             HomeViewCommand = new RelayCommand(() => ExecuteHomeViewCommand());
+            MMAboutViewCommand = new RelayCommand(() => ExecuteMMAboutViewCommand());
+            MMAddToQueueViewCommand = new RelayCommand(() => ExecuteMMAddToQueueViewCommand());
+            MMConfigureCPViewCommand = new RelayCommand(() => ExecuteMMConfigureCPViewCommand());
+            MMPlayerSignInViewCommand = new RelayCommand(() => ExecuteMMPlayerSignInViewCommand());
+            MMViewActiveGamesViewCommand = new RelayCommand(() => ExecuteMMViewActiveGamesViewCommand());
+            MMViewPPViewCommand = new RelayCommand(() => ExecuteMMViewPPViewCommand());
         }
 
     }
