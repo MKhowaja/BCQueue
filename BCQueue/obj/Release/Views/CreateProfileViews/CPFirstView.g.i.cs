523bbe018f7c6ea5474afd57734d1f4bb1f408fe
Clement Hoang <clement.hoang24@gmail.com>
2013-08-08 11:14:43 -0400

    good to go

--- BCQueue/obj/Release/Views/CreateProfileViews/CPFirstView.g.i.cs
+++ BCQueue/obj/Release/Views/CreateProfileViews/CPFirstView.g.i.cs
@@ -9,7 +9,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using BCQueue;
 using System;
 using System.Diagnostics;
 using System.Windows;
